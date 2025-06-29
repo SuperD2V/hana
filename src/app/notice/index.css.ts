8626fa8a11a6f48148234a3b8d5a636c4927d343
jyh2610 <jogg7191@gmail.com>
2025-06-30 03:12:12 +0900

    공지및 주보

---
+++ src/app/notice/index.css.ts
@@ -0,0 +1,9 @@
+import { color } from "@/component/shared/designed/color";
+import { style } from "@vanilla-extract/css";
+
+export const container = style({
+  display: "flex",
+  flexDirection: "column",
+  height: "100%",
+  background: color.brand_yellow[1]
+});
