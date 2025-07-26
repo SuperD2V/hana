18bfae529f8d6924146747f0e7af524f6475291d
jyh2610 <jogg7191@gmail.com>
2025-07-27 01:19:11 +0900

    인증로직 추가

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,18 +1,21 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import AdminNavigation from "@/component/admin/AdminNavigation";
 import AdminContent from "@/component/admin/AdminContent";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 
-const page = async () => {
-  // 쿠키에서 토큰 확인
-  const cookieStore = await cookies();
-  const token = cookieStore.get("token");
+const page = () => {
+  const router = useRouter();
 
-  // 토큰이 없으면 로그인 페이지로 리다이렉트
-  if (!token) {
-    redirect("/admin/signin");
-  }
+  useEffect(() => {
+    // 로컬스토리지에서 accessToken 확인
+    const accessToken = localStorage.getItem("accessToken");
+
+    // 토큰이 없으면 로그인 페이지로 리다이렉트
+    if (!accessToken) {
+      router.push("/admin/signin");
+    }
+  }, [router]);
 
   return (
     <div style={{ display: "flex", gap: "80px" }}>
